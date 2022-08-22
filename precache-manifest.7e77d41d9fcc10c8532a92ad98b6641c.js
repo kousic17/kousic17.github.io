e3c9c64954311f4fc7101960ccb577d40c006de4
Kousic <kousic.it@gmail.com>
2022-08-22 13:32:19 +0530

    Updates

---
+++ precache-manifest.7e77d41d9fcc10c8532a92ad98b6641c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc2f3b594396f632bd75",
+    "url": "/static/css/main.03e2d41f.chunk.css"
+  },
+  {
+    "revision": "fc2f3b594396f632bd75",
+    "url": "/static/js/main.9d4a1000.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23dc5f7aa5c3783eb245",
+    "url": "/static/css/2.4c7d8947.chunk.css"
+  },
+  {
+    "revision": "23dc5f7aa5c3783eb245",
+    "url": "/static/js/2.6fae5b63.chunk.js"
+  },
+  {
+    "revision": "f043c15ddb6c187bb20924757190e48d",
+    "url": "/static/media/Kousic.f043c15d.jpg"
+  },
+  {
+    "revision": "285befdea0af9b6a1f04f4d348619bc8",
+    "url": "/static/media/img.285befde.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "cf317dac6ada6b118e6ecf204c1d0003",
+    "url": "/index.html"
+  }
+];
